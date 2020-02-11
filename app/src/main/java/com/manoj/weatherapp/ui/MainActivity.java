775ca97f5dc63d46b3ae063e7f4703c07c3e8cb3
Manoj Kumar2 <manoj.kumar2@mavenir.com>
2020-02-11 19:04:09 +0530

    Added searching city.

--- app/src/main/java/com/manoj/weatherapp/ui/MainActivity.java
+++ app/src/main/java/com/manoj/weatherapp/ui/MainActivity.java
@@ -1,7 +1,12 @@
 package com.manoj.weatherapp.ui;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,7 +30,13 @@ public class MainActivity extends AppCompatActivity {
         mMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mWeatherViewModel = ViewModelProviders.of(this).get(WeatherViewModel.class);
         wait(true);
-        fetchWeatherDetails("Bangalore");
+        fetchWeatherDetails("Bangalore", false);
+
+        mMainBinding.layout.txtChangeCity.setOnClickListener(v -> searchCity());
+        mMainBinding.txtError.setOnClickListener(v -> {
+            wait(true);
+            fetchWeatherDetails("Bangalore", false);
+        });
 
     }
 
@@ -33,14 +44,16 @@ public class MainActivity extends AppCompatActivity {
         if (isloading) {
             mMainBinding.progressBar.setVisibility(View.VISIBLE);
             mMainBinding.layout.rlRoot.setVisibility(View.GONE);
+            mMainBinding.txtError.setVisibility(View.GONE);
         } else {
             mMainBinding.progressBar.setVisibility(View.GONE);
             mMainBinding.layout.rlRoot.setVisibility(View.VISIBLE);
+            mMainBinding.txtError.setVisibility(View.GONE);
         }
     }
 
-    private void fetchWeatherDetails(String cityName) {
-        mWeatherViewModel.getCurrentWeatherLiveData(cityName).observe(this, result -> {
+    private void fetchWeatherDetails(String cityName, boolean isSearching) {
+        mWeatherViewModel.getCurrentWeatherLiveData(cityName).observeForever( result -> {
             if (result != null) {
                 wait(false);
                 mMainBinding.layout.txtCityName.setText(result.getLocation().getName());
@@ -49,13 +62,50 @@ public class MainActivity extends AppCompatActivity {
                 mMainBinding.layout.txtFeelLike.setText(result.getCurrent().getFeelslike() + "\u2103");
                 mMainBinding.layout.txtHumidity.setText(String.valueOf(result.getCurrent().getHumidity()));
                 mMainBinding.layout.txtPrecipitation.setText(result.getCurrent().getPrecip() + "%");
+                mMainBinding.layout.txtWeatherDesc.setText(result.getCurrent().getWeatherDescriptions().get(0));
                 Glide.with(this)
                         .load(result.getCurrent().getWeatherIcons().get(0))
                         .into(mMainBinding.layout.imgWeatherIc);
             } else {
-                //todo: handle error
+                if (isSearching) {
+                    wait(false);
+                    errorOccur();
+                } else {
+                    wait(false);
+                    errorOccur();
+                    mMainBinding.txtError.setText(R.string.server_error);
+                }
             }
         });
     }
 
+    private void errorOccur() {
+        mMainBinding.txtError.setVisibility(View.VISIBLE);
+        mMainBinding.progressBar.setVisibility(View.GONE);
+        mMainBinding.layout.rlRoot.setVisibility(View.GONE);
+    }
+
+    private void searchCity() {
+
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View changeCityView = factory.inflate(R.layout.layout_dialog, null);
+        final AlertDialog changeCityDialog = new AlertDialog.Builder(this).create();
+        changeCityDialog.setView(changeCityView);
+
+        EditText editText = changeCityView.findViewById(R.id.et_city);
+
+        changeCityView.findViewById(R.id.txt_ok).setOnClickListener(v -> {
+            String cityName = editText.getText().toString();
+            if (TextUtils.isEmpty(cityName)) {
+                Toast.makeText(this, R.string.valid_city, Toast.LENGTH_SHORT).show();
+            } else {
+                wait(true);
+                fetchWeatherDetails(cityName, true);
+                changeCityDialog.dismiss();
+            }
+        });
+        changeCityView.findViewById(R.id.txt_cancel).setOnClickListener(v -> changeCityDialog.dismiss());
+        changeCityDialog.show();
+
+    }
 }
